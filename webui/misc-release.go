2dfd3cff3d0b12f53d14cba9fa326b1f80bd8d79
TQ Hirsch <thequux@upstandinghackers.com>
2015-01-11 16:35:10 -0600

    Fixed build in release mode

--- webui/misc-release.go
+++ webui/misc-release.go
@@ -1,8 +1,12 @@
-// +build -debug
+// +build !debug
 
 package webui
 
 import "github.com/gin-gonic/gin"
+
+func init() {
+	gin.SetMode("release")
+}
 
 func TemplateReloader(ctx *gin.Context) {
 	ctx.Next()
